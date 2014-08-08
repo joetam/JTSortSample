ef12aecdad0422e8b829f78b0f86bc0cbf2621ab
Joe Tam <joe611@gmail.com>
2014-08-07 22:27:14 -0400

    Replaced Insertion Sort implementation with in-place implementation

--- Classes/sorts/InsertionSort.m
+++ Classes/sorts/InsertionSort.m
@@ -10,19 +10,43 @@
 
 @implementation InsertionSort
 
+// not in-place
+//- (NSArray *)sort:(NSArray *)list
+//{
+//    NSMutableArray *result = [[NSMutableArray alloc] init];
+//    
+//    for (NSNumber *num in list) {
+//        int i = result.count;
+//        while (i > 0 && [num compare:[result objectAtIndex:i-1]] == NSOrderedAscending) {
+//            i--;
+//        }
+//        [result insertObject:num atIndex:i];
+//    }
+//    return result;
+//}
+
+// in-place implementation
 - (NSArray *)sort:(NSArray *)list
 {
-    NSMutableArray *result = [[NSMutableArray alloc] init];
+    NSMutableArray *result = [NSMutableArray arrayWithArray:list];
     
-    for (NSNumber *num in list) {
-        int i = result.count;
-        while (i > 0 && [num compare:[result objectAtIndex:i-1]] == NSOrderedAscending) {
-            i--;
+    for (int i = 0; i < result.count; i++) {
+        int j = i;
+        NSNumber *minValue = result[j];
+        while (j > 0 && [result[j-1] compare: minValue] == NSOrderedDescending) {
+            swap(result, j, j-1);
+            j--;
         }
-//        if (i < 0) i = 0;
-        [result insertObject:num atIndex:i];
     }
+    
     return result;
+}
+
+void swap(NSMutableArray *array, int firstIndex, int secondIndex)
+{
+    id firstObj = [array objectAtIndex:firstIndex];
+    [array setObject:[array objectAtIndex:secondIndex] atIndexedSubscript:firstIndex];
+    [array setObject:firstObj atIndexedSubscript:secondIndex];
 }
 
 - (NSString *)sortMethodName
